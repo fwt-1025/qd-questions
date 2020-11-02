d85ca196e03e53c44c3aefb86ca80069a69d6060
Wentao.Fu <36839267+ComponentTY@users.noreply.github.com>
2020-11-02 19:22:53 +0800

    Update websocket中转.js

--- websocket中转.js
+++ websocket中转.js
@@ -1,11 +1,27 @@
 const WServer = require('ws').Server
 const Websocket = require('ws')
 const conf = require('../config/index')(process.env.NODE_ENV) // 根据环境获取不同的websocketURL
+// config/index.js
+//    export default {
+//         dev: {
+//              websocket: {
+//                     faultServer: 'xx.xx.xx.xx:8080/server', // java端的websocket服务地址，真实的地址
+//                     upgradeServer: 'xx.xx.xx.xx:8081/upserver'
+//             }
+//         },
+//         prod: {
+//              websocket: {
+//                     faultServer: 'xx.xx.xx.xx:8080/server',
+//                     upgradeServer: 'xx.xx.xx.xx:8081/upserver'
+//             }
+//         }
+//     }
 let wsClient = {} // 存储客户端websocket连接
 let clientFront = {} // 存储node端的客户端websocket连接
 function webSocketServer(server) {
     const wsServer = new WServer({ server })
     wsServer.on('connection', (conn, req) => {
+//         浏览器端连接时需要在链接后面拼上对应的服务 ws://xx.xx.xx.xx:8080/10110?faultServer  // 连接的node 本地服务的地址， 基于express 与express服务共用一个端口。
         let url = req.url.split('?')
         if (wsClient[req.url]) {
             delete wsClient[req.url]
@@ -15,7 +31,7 @@ function webSocketServer(server) {
         wsClient[req.url].on('message', (mess) => {
         })
         wsClient[req.url].on('close', () => {
-            clientFront[req.url].close() // when the browser client is closed, the node client should also be shut down
+            clientFront[req.url].close() // when the browser client is closed, the node client also should be shut down, 解决内存泄漏， 不然接收到的数据无法被消费掉。
             delete clientFront[req.url]
             return false
         })
